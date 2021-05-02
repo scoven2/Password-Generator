e6d938187f2b7225338890b19275ffc64db747fa
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-05-01 21:46:56 -0500

    added beginning of function

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ get.addEventListener("click", function () {
     ps = generatePassword();
     document.getElementById("password").placeholder = ps;
 });
+
+// generate password function
+function generatePassword() {
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    if (!enter) {
+        alert("Please enter a value");
+    } else if (enter < 8 || enter > 128) {
+        //validates that right value is entered
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+    //once validated asking yes or no to other options
+    } else {
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmNumber = confirm("WIll this contain numbers?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?")
+    };
+}
